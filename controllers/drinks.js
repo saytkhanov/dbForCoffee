41b87d4a0fdc201bc7bf12c1143ad3e41497923f
saytkhanov <saythanov2014@mail.ru>
2021-06-16 14:02:15 +0300

    add route

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -1,13 +1,13 @@
-const Drink = require('../models/Drink')
+const Drink = require("../models/Drink");
 
 const controllers = {
   getAllDrinks: async (req, res) => {
-    const getAllDrinks = await Drink.find({ }, { name: 1, cost: 1 } )
-    res.json(getAllDrinks)
+    const getAllDrinks = await Drink.find({}, { name: 1, cost: 1 });
+    res.json(getAllDrinks);
   },
   getDrinksById: async (req, res) => {
-    const getDrinksById = await Drink.findById(req.params.id)
-    res.json(getDrinksById)
+    const getDrinksById = await Drink.findById(req.params.id);
+    res.json(getDrinksById);
   },
   postDrinks: async (req, res) => {
     const postDrinks = await new Drink({
@@ -16,28 +16,32 @@ const controllers = {
       inStock: req.params.inStock,
       hasItCaffeine: req.body.hasItCaffeine,
       volume: req.body.volume,
-      description: req.body.description
-    })
-    await postDrinks.save()
-    res.json(postDrinks)
+      description: req.body.description,
+    });
+    await postDrinks.save();
+    res.json(postDrinks);
   },
   deleteDrink: async (req, res) => {
-    const deleteTodo = await Drink.findByIdAndDelete(req.params.id)
-    res.json(deleteTodo)
+    const deleteTodo = await Drink.findByIdAndDelete(req.params.id);
+    res.json(deleteTodo);
   },
   patchDrink: async (req, res) => {
     const id = req.params.id;
-    const {name, cost, inStock, hasItCaffeine, volume, description} = req.body
-    const options = {new: true}
-    const patchDrink = await Drink.findByIdAndUpdate(id, {name, cost, inStock, hasItCaffeine, volume, description}, options)
-    await patchDrink.save()
-    res.json(patchDrink)
+    const { name, cost, inStock, hasItCaffeine, volume, description } =
+      req.body;
+    const options = { new: true };
+    const patchDrink = await Drink.findByIdAndUpdate(
+      id,
+      { name, cost, inStock, hasItCaffeine, volume, description },
+      options
+    );
+    await patchDrink.save();
+    res.json(patchDrink);
   },
   getInStockDrink: async (req, res) => {
-    const getInStockDrink = await Drink.find({inStock: true})
-    res.json(getInStockDrink)
-  }
-}
+    const getInStockDrink = await Drink.find({inStock: true});
+    res.json(getInStockDrink);
+  },
+};
 
-
-module.exports = controllers
+module.exports = controllers;
--- routes/drinks.js
+++ routes/drinks.js
@@ -1,13 +1,19 @@
-const { Router } = require('express');
-const router = Router()
-const { getAllDrinks, postDrinks, deleteDrink, patchDrink, getInStockDrink, getDrinksById} = require('../controllers/drinks');
+const { Router } = require("express");
+const router = Router();
+const {
+  getAllDrinks,
+  postDrinks,
+  deleteDrink,
+  patchDrink,
+  getInStockDrink,
+  getDrinksById,
+} = require("../controllers/drinks");
 
+router.get("/drinks/stock", getInStockDrink);
+router.get("/drinks", getAllDrinks);
+router.post("/drinks", postDrinks);
+router.get("/drinks/:id", getDrinksById);
+router.delete("/drinks/:id", deleteDrink);
+router.patch("/drinks/:id", patchDrink);
 
-router.get('/drinks', getAllDrinks)
-router.post('/drinks', postDrinks);
-router.get('/drinks/:id',getDrinksById );
-router.delete('/drinks/:id', deleteDrink);
-router.patch('/drinks/:id', patchDrink)
-router.get('/drinks/in-stock', getInStockDrink)
-
-module.exports = router
+module.exports = router;
